b8c4fe9fa9ec118c608900c74576b2e0994fb339
JayaPrakash-P <55636787+JayaPrakash-P@users.noreply.github.com>
2020-12-26 18:09:01 +0530

    Initial commit

---
+++ LinuxDev/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <pthread.h>
+
+int main()
+{
+    printf("hello from %s!\n", "LinuxDev1");
+    printf("hello from %s!\n", "LinuxDev2");
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# wsl-repository
+ WSL Projects Repository
