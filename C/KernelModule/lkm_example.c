86880454d603a8d5fefa51a3b928859571ba024b
JayaPrakash-P <jayaprakash.p@outlook.com>
2022-02-18 01:00:22 +0530

    Kernel Module - Build fix for WSL2

--- C/KernelModule/lkm_example.c
+++ C/KernelModule/lkm_example.c
@@ -1,4 +1,10 @@
-//https://unix.stackexchange.com/questions/594470/wsl-2-does-not-have-lib-modules
+/*JP:~/WIP/GITRepos/wsl-repository/C/KernelModule$ make
+make -C /lib/modules/5.10.60.1-microsoft-standard-WSL2/build M=/home/jp/WIP/GITRepos/wsl-repository/C/KernelModule modules
+make[1]: *** /lib/modules/5.10.60.1-microsoft-standard-WSL2/build: No such file or directory.  Stop.
+make: *** [Makefile:3: all] Error 2
+
+Fix  for WSL2 : https://unix.stackexchange.com/questions/594470/wsl-2-does-not-have-lib-modules*/
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
